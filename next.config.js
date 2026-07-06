a0716fb6be10bd8777d5d9f57cc1413207cb5ce2
mmakanda <mmakanda@users.noreply.github.com>
2026-07-06 06:07:49 +0200

    Revert "Add custom Clerk domain to CSP directives"
    This reverts commit 49db182d182f8535f540db6c03d5452403785112.

--- next.config.js
+++ next.config.js
@@ -25,12 +25,12 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.accounts.dev https://*.clerk.com https://*.boq.amaryllissuccess.co.zw https://challenges.cloudflare.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.accounts.dev https://*.clerk.com https://challenges.cloudflare.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: blob: https://*.clerk.com https://img.clerk.com https://*.boq.amaryllissuccess.co.zw",
-              "connect-src 'self' https://amaryllis-boq-production.up.railway.app https://*.clerk.accounts.dev https://*.clerk.com https://*.boq.amaryllissuccess.co.zw",
-              "frame-src 'self' https://challenges.cloudflare.com https://*.clerk.accounts.dev https://*.clerk.com https://*.boq.amaryllissuccess.co.zw",
+              "img-src 'self' data: blob: https://*.clerk.com https://img.clerk.com",
+              "connect-src 'self' https://amaryllis-boq-production.up.railway.app https://*.clerk.accounts.dev https://*.clerk.com",
+              "frame-src 'self' https://challenges.cloudflare.com https://*.clerk.accounts.dev https://*.clerk.com",
               "worker-src 'self' blob:",
             ].join('; '),
           },
@@ -43,4 +43,5 @@ const nextConfig = {
     ];
   },
 };
+
 module.exports = nextConfig;
